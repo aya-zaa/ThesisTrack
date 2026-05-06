042bbf1c08fbc8ebcf467dfebf1d0af7c9bbb381
aya <211205990+aya-zaa@users.noreply.github.com>
2026-05-06 14:29:48 +0100

    Delete api directory

--- api/Connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "thesis");
-
-if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
-}
-
-mysqli_set_charset($conn, "utf8");
-?>
